368910b0ac5ecba8c7a4816426bfb82553875e2d
Smeby33 <smebedoh33@gmail.com>
2024-11-01 11:56:58 +0100

    first

---
+++ src/connexion.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Connexion() {
+  // Définir un état pour la visibilité du formulaire
+  const [showForm, setShowForm] = useState(false);
+
+  // Fonction pour basculer la visibilité
+  const handleConnexionClick = () => {
+    setShowForm(!showForm);
+  };
+
+  return (
+    <div className='Connexion'>
+        <div className="divbtn">
+            <button className='btnconx' onClick={handleConnexionClick}>
+                <h1>
+                    Connexion
+                </h1> 
+            </button>
+        </div>
+      
+
+      {/* Afficher le formulaire seulement si showForm est true */}
+      {showForm && (
+        <div className="formu">
+        <div className="nomUser">
+          <label htmlFor="username">Nom d'utilisateur<span>*</span></label>
+          <input type="text" id="username" placeholder="Nom d'utilisateur" />
+        </div>
+        <div className="passeWord">
+          <label htmlFor="password">Mot de passe<span>*</span></label>
+          <input type="password" id="password" placeholder="Mot de passe" />
+        </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Connexion;
